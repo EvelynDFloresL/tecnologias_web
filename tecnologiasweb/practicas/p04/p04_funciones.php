23990a9abb0d3cffcf16f1dcbb13cc708622fdf9
evelyndfloresl <eve_flor17@outlook.com>
2023-09-16 17:13:02 -0600

    Ejercicio 2 de la práctica 4 completo

--- tecnologiasweb/practicas/p04/p04_funciones.php
+++ tecnologiasweb/practicas/p04/p04_funciones.php
@@ -8,3 +8,32 @@ function multiplo($numero)
         echo "El número $numero no es múltiplo de 5 y 7 <br>";
     }
 }
+
+
+function secuencia() {
+    $matriz = [];
+    $iteraciones = 0;
+
+    while (true) {
+        $numeros = [];
+        for ($i = 0; $i < 3; $i++) {
+            $num = rand(1, 100);
+            $numeros[] = $num;
+        }
+        $iteraciones++;
+        if ($numeros[0] % 2 == 1 && $numeros[1] % 2 == 0 && $numeros[2] % 2 == 1) {
+            $matriz[] = $numeros;
+            break;
+        }
+        $matriz[] = $numeros;
+    }
+    echo "Matriz de numeros: <br>";
+    foreach ($matriz as $fila) {
+        echo implode("\t", $fila) . "<br>";
+    }
+    echo "<br>Número de Iteraciones: $iteraciones";
+    echo "<br>Cantidad de Números Generados: " . count($matriz) * 3;
+}
+
+
+
